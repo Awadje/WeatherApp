115dbb31605fbd357512990c46c728eb1a3063ac
Awadje <michaelawad85@gmail.com>
2017-06-11 17:14:57 +0200

    Mapped over array to return an array of temperatures, which serves as input for sparklines

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp)
+    console.log(temps);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    )
+  }
+
+
   render() {
     return (
       <table className="table table-hover">
@@ -14,6 +27,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
