f457d6e21ff4720e8d322c3f2d1728634da8f61a
Awadje <michaelawad85@gmail.com>
2017-06-11 16:13:02 +0200

    Connected onFormSubmit to fetchweather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='50ac305e6b4c6e015acd4e0a3e9f297e';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,6 +11,7 @@ class SearchBar extends Component {
     this.state = { term: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
@@ -20,7 +21,8 @@ class SearchBar extends Component {
 
   onFormSubmit(event) {
     event.preventDefault();
-
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
 
