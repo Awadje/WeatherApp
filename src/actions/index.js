de46091345b66d8209bd653ad761e1c2160babb3
Awadje <michaelawad85@gmail.com>
2017-06-11 15:49:15 +0200

    Create API Request URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
-const API_KEY ='50ac305e6b4c6e015acd4e0a3e9f297e'
+const API_KEY ='50ac305e6b4c6e015acd4e0a3e9f297e';
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},nl`;
+  return {
+    type: FETCH_WEATHER
+  };
+}
